9eef5603f6699ba1091d2fc91f0a3e0920ab0db0
Nitin1130 <nitinchoudhary260@gmail.com>
2023-03-03 12:01:36 +0530

    Added spring batch

---
+++ src/main/java/com/spring/batch/springbatchpracticeproject/SpringBatchPracticeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.batch.springbatchpracticeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchPracticeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchPracticeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/batch/springbatchpracticeproject/SpringBatchPracticeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.batch.springbatchpracticeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchPracticeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
